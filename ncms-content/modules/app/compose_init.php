26e668d5f87d08ec8ac99d5fc041e89efc429c9f
Axton <enquires@axtonprice.com>
2022-04-05 04:03:59 +0100

    commit

--- ncms-content/modules/app/compose_init.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (strpos($_SERVER['REQUEST_URI'], "ncms-system/install") == false) {
-    if (strpos($_SERVER['REQUEST_URI'], "ncms-system/error") == false) {
-        initCheckStartup();
-        initCheckInstallKey();
-    }
-}
-?>
